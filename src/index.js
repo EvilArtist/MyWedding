03f5e3e10be420229de4db774fbe229f6f123305
Evil Artist <Loc.NguyenXuan99@outlook.com>
2022-06-17 08:09:32 +0700

    Add library

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import './library/js-image-slider.css';
+import "./library/js-image-slider";
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
