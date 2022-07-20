09cf6301c06c3eb7e3df03b321667a445f808b33
EvilArtist <loc.nguyenxuan99@outlook.com>
2022-07-20 08:41:22 +0700

    Remove test cmp

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
         <div className="invitation-card-viewer">
             <FlipCard />
         </div>
-        <InvitationCard />
+
         <svg className="svg">
             <clipPath id="heart" clipPathUnits="objectBoundingBox">
             <path d="M0.5,0.16 C0.448,0.066,0.362,0,0.259,0 C0.112,0,0,0.123,0,0.283 C0,0.594,0.155,0.642,0.5,1 C0.845,0.642,1,0.594,1,0.283 C1,0.123,0.888,0,0.741,0 C0.638,0,0.552,0.066,0.5,0.16"></path>
