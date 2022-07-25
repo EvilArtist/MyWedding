a6d2fdab3f2aa11e9ccc38db5a34014208f2051f
Evil Artist <Loc.NguyenXuan99@outlook.com>
2022-07-25 12:28:46 +0700

    Done Card

--- src/components/PartyCard.js
+++ src/components/PartyCard.js
@@ -11,10 +11,8 @@ class PartyCard extends React.Component {
 
     render() {
         return <div className="party-card">
-            <div className="corner-image">
-                <div className="image-container">
-                    <img src={process.env.PUBLIC_URL + "/images/corner.png"}  alt="" />
-                </div>
+            <div className="background-image">
+                <img src={process.env.PUBLIC_URL + "/images/background.png"}  alt="" />
             </div> 
             <div className="invitation-card-inner">
                 <div className="invitation-title">
@@ -26,18 +24,8 @@ class PartyCard extends React.Component {
                 <div className="invitation-content">
                     Đến dự lễ báo hỷ của hai chúng tôi
                 </div>
-                <div className="groom-bride-name">
-                    <div className='image-border'>
-                        <img src={process.env.PUBLIC_URL + "/images/cart-name-border.png"}  alt="" />
-                    </div>
-                    <div className="groom-name hw">Xuân Lộc</div>
-                    <div className="heart-container">
-
-                    <div className='heart'>&#10084;</div>
-                    </div>
-                    <div className="bride-name hw">Quỳnh Ái</div>
-                </div>
-                    <div className="titlte">Được tổ chức vào lúc 18:00</div>
+               
+                <div className="titlte">Được tổ chức vào lúc 18:00</div>
                 <div className="invitation-time-address">
                     <div className="invitation-time">
                         <div className="day">Chủ nhật</div>
@@ -46,14 +34,23 @@ class PartyCard extends React.Component {
                         <div className="lunar-date">(15-08)</div>
                     </div>
                     <div className="between-sepacrator"></div>
-                    <a className="address" target="blank" href='https://www.google.com/maps/dir/?api=1&destination=10.749961465233808, 106.63532412669963'>
+                    <div className="address" >
                         <div className='floor'>Sảnh Paris, Lầu 2<br></br> Nhà hàng tiệc cưới</div>
                         <div className='restaurant'>Vân Nghĩa</div>
                         <div className='full-address'>333 Đ. Nguyễn Văn Luông, Phường 12, Quận 6, Thành phố Hồ Chí Minh
                             
                         </div>
-                        <span className='icon-address' ><img src={process.env.PUBLIC_URL + "/images/mapicon.png"}  alt="" /></span>
-                    </a>
+                        <div className='icons'>
+                            <a target="blank" href='http://www.vannghiapalace.com/sanh-tiec/sanh-rome.html'
+                                 title='Trang web' > 
+                                <span className='icon-address' ><img src={process.env.PUBLIC_URL + "/images/restaurant.png"}  alt="" />
+                                </span>
+                            </a>
+                            <a target="blank" title='Chỉ đường' 
+                                href='https://www.google.com/maps/dir/?api=1&destination=10.749961465233808, 106.63532412669963'> <span className='icon-address' ><img src={process.env.PUBLIC_URL + "/images/mapicon.png"}  alt="" /></span></a>
+                        </div>
+                       
+                    </div>
                 </div>
                 <div className="wellcome hw">Sự hiện diện của anh là niềm vinh hạnh <br/> cho gia đình chúng tôi</div>
             </div>
