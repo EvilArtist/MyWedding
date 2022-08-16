c46ca81b26fe4923c9e897a3d2223a10db3c5c50
Evil Artist <Loc.NguyenXuan99@outlook.com>
2022-08-16 20:00:05 +0700

    Done

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Header from "./components/Header";
 import React, { useState } from 'react';
 import HowWeKnow from './components/HowWeKnow';
 import Upcomming from './components/Upcomming';
+import LoveGrowup from './components/LoveGrowUp';
 
 function App() {
     const [cardShow, setCardShow] = useState(false);
@@ -23,7 +24,16 @@ function App() {
             </div>}
 
             <HowWeKnow />
-
+            <LoveGrowup />
+            <div className="story-container poem-container">
+                <div className="story-header">
+                    Cảm ơn tất cả đã đem chúng tôi đến bên nhau
+                </div>
+                <div className='video-container'>
+                    <iframe width="100%" src="https://www.youtube.com/embed/pYBZRz2H6tc?autoplay=1" 
+                    title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+                </div>
+            </div>
             <svg className="svg">
                 <clipPath id="heart" clipPathUnits="objectBoundingBox">
                     <path d="M0.5,0.16 C0.448,0.066,0.362,0,0.259,0 C0.112,0,0,0.123,0,0.283 C0,0.594,0.155,0.642,0.5,1 C0.845,0.642,1,0.594,1,0.283 C1,0.123,0.888,0,0.741,0 C0.638,0,0.552,0.066,0.5,0.16"></path>
--- src/components/LoveGrowUp.js
+++ src/components/LoveGrowUp.js
@@ -1,32 +1,35 @@
+import './LoveGrowup.css';
 function LoveGrowup() {
-    return <div className="story-container">
+    return <div className="story-container poem-container">
     <div className="story-header">
-    Chúng tôi đã gặp nhau như thế nào    
+        Tình yêu dần lớn lên  
     </div>
-    <div className="story-grid">
+    <div className="image-cover">
         <div className="image">
-            <img src="/images/story/chat.webp" alt="" />
+            <img src="/images/slide/Bia.jpg" alt="" />
         </div>
-        <div className="text">
-        <p>Anh muốn được cùng em</p>
-        <p>Đi qua những gian khổ</p>
-        <p>Cuộc đời xô ta đổ</p>
-        <p>Ta dìu nhau đứng lên.</p> <br />
-        <p>Khi hơi thở vào đêm</p>
-        <p>Nỗi buồn kia cũ lắm</p>
-        <p>Bàn tay là chăn ấm</p>
-        <p>Ru em giấc ngủ yên.</p>  <br />
-        <p>Anh muốn được cùng em</p>
-        <p>Đi qua những mùa nhớ</p>
-        <p>Tận cùng của nỗi sợ</p>
-        <p>Là khi mình cô đơn.</p>  <br />
-        <p>Mình cùng ngắm hoàng hôn</p>
-        <p>Ở hai đầu thành phố</p>
-        <p>Anh gửi thơ vào gió</p>
-        <p>Em gửi hương vào mây.</p>  <br />
-
+        <div className="text poem">
+            <div className="poem-content">
+                <p>Anh muốn được cùng em</p>
+                <p>Đi qua những gian khổ</p>
+                <p>Cuộc đời xô ta đổ</p>
+                <p>Ta dìu nhau đứng lên.</p> <br />
+                <p>Khi hơi thở vào đêm</p>
+                <p>Nỗi buồn kia cũ lắm</p>
+                <p>Bàn tay là chăn ấm</p>
+                <p>Ru em giấc ngủ yên.</p>  <br />
+                <p>Anh muốn được cùng em</p>
+                <p>Đi qua những mùa nhớ</p>
+                <p>Tận cùng của nỗi sợ</p>
+                <p>Là khi mình cô đơn.</p>  <br />
+                <p>Mình cùng ngắm hoàng hôn</p>
+                <p>Ở hai đầu thành phố</p>
+                <p>Anh gửi thơ vào gió</p>
+                <p>Em gửi hương vào mây.</p>  <br />
+            </div>
         </div>
     </div>
 
 </div>
 }
+export default LoveGrowup;
